b2e065e35669706ea0f2a9a2d27d2022859b64de
flyingcoder-2 <reyanshcoder1234@gmail.com>
2025-02-07 14:50:22 +0800

    added contact form destination email

--- mailer/forms_settings.php
+++ mailer/forms_settings.php
@@ -1,8 +1,8 @@
 <?php
 	/* Forms settings */
 	$subj = "New message from the site 'Genkiz - Portfolio Agency Template'"; //letter subject
-	$to = 'yourname@domain.com'; // Enter Your E-mail
-	$from = 'admin@domain.com'; // Admin e-mail
+	$to = 'thegenkizkhan@gmail.com'; // Enter Your E-mail
+	$from = 'admin@genkiz.com'; // Admin e-mail
 	$fromName = 'Genkiz'; // Your company name
 	$charset = 'UTF-8';
 ?>
